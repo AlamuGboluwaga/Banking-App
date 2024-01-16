5b732f0ae9ad6611688b2d28d48cf1369716abcb
AlamuGboluwaga <alamugboluwaga402@gmail.com>
2024-01-16 04:11:58 +0100

    Dashboard : Update

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
 const Dashboard = () => {
-  return <div>This the Dashboard</div>;
+  return (
+    <div className=" grid grid-cols-2  gap-[10px]  justify-center items-center grid-tem grid-auto-rows minmax(100px, h-auto) border border-[solid] border-red-500   ">
+      <div>
+        <input type="button" value="Transfer" />
+      </div>
+      <div>
+        <input type="button" value="Withdrawer" />
+      </div>
+      <div>
+        <input type="button" value="Balance" />
+      </div>
+      <div>
+        <input type="button" value="Deposit" />
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
