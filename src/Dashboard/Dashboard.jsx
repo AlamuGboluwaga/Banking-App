e460e88f9b748d7c6e85f776eee26c243622b6f7
AlamuGboluwaga <alamugboluwaga402@gmail.com>
2024-01-18 07:59:34 +0100

    API :Consumtion

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -2,18 +2,21 @@ import React from "react";
 
 const Dashboard = () => {
   return (
-    <div className=" grid grid-cols-2  gap-[10px]  justify-center items-center grid-tem grid-auto-rows minmax(100px, h-auto) border border-[solid] border-red-500   ">
-      <div>
-        <input type="button" value="Transfer" />
+    <div className=" grid grid-cols-2  gap-[10px]  justify-center items-center grid-tem grid-auto-rows minmax(100px, h-auto) border border-[solid] border-red-500    ">
+      <div className=" bg-amber-50 h-200 h-[200px] flex justify-center items-center ">
+        Create New Custmer
       </div>
-      <div>
-        <input type="button" value="Withdrawer" />
+      <div className=" bg-amber-50 h-200 h-[200px] flex justify-center items-center ">
+        Deposit
       </div>
-      <div>
-        <input type="button" value="Balance" />
+      <div className=" bg-amber-50 h-200 h-[200px] flex justify-center items-center ">
+        Withdraw
       </div>
-      <div>
-        <input type="button" value="Deposit" />
+      <div className=" bg-amber-50 h-200 h-[200px] flex justify-center items-center ">
+        Transfer
+      </div>
+      <div className=" bg-amber-50 h-200 h-[200px] flex justify-center items-center ">
+        Balance
       </div>
     </div>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,146 @@
-import React from 'react'
+import * as React from "react";
+import { styled } from "@mui/material/styles";
+import Card from "@mui/material/Card";
+import CardHeader from "@mui/material/CardHeader";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import CardActions from "@mui/material/CardActions";
+import Collapse from "@mui/material/Collapse";
+import Avatar from "@mui/material/Avatar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import { red } from "@mui/material/colors";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ShareIcon from "@mui/icons-material/Share";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import axios from 'axios'
 
-const Home = () => {
-  return (
-    <div className='bg-blue-100 h-screen w-screen' >
-      Home
-    </div>
-  )
+const ExpandMore = styled((props) => {
+  const { expand, ...other } = props;
+  return <IconButton {...other} />;
+})(({ theme, expand }) => ({
+  transform: !expand ? "rotate(0deg)" : "rotate(180deg)",
+  marginLeft: "auto",
+  transition: theme.transitions.create("transform", {
+    duration: theme.transitions.duration.shortest,
+  }),
+}));
+
+ const Home =()=>{
+  const [expanded, setExpanded] = React.useState(false);
+
+  const handleExpandClick = () => {
+    setExpanded(!expanded);
+  };
+
+  const [data,setData] =React.useState([])
+  const handleButton = () => {
+
+  };
+
+  const fetchData= async()=>{
+try {
+      const response = await axios.get("https://dummyjson.com/products");
+      setData(response.data.products);
+      console.log(response.data.products);
+} catch (error) {
+  console.log("CHECK ERROR",error.message);
 }
-
+  }
+  React.useEffect(()=>{
+    fetchData()
+  },[])
+  return (
+    <div className=" m-24  gap-20 flex flex-row flex-wrap bg-pink-100 ">
+      {data.map((items, index) => (
+        <div key={index} className=" ">
+          <Card sx={{ maxWidth: 345 }}>
+            <CardHeader
+              avatar={
+                <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                  {items.stock}
+                </Avatar>
+              }
+              action={
+                <IconButton aria-label="settings">
+                  <MoreVertIcon />
+                </IconButton>
+              }
+              title={items.title}
+              subheader={items.description}
+            />
+            <CardMedia
+              component="img"
+              height="194"
+              image={items.images[0]}
+              alt="Paella dish"
+            />
+            <CardContent>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                className="flex flex-col gap-6 justify-center  items-center"
+              >
+                Price : ${items.price}
+                <button className="bg-gray-400 active:bg-gray-300 rounded-[5px] pb-[10px] pt-[10px] pr-[5px] pl-[5px] w-[12rem] text-white">
+                  Add
+                </button>
+              </Typography>
+            </CardContent>
+            <CardActions disableSpacing>
+              <IconButton aria-label="add to favorites">
+                <FavoriteIcon />
+              </IconButton>
+              <IconButton aria-label="share">
+                <ShareIcon />
+              </IconButton>
+              <ExpandMore
+                expand={expanded}
+                onClick={handleExpandClick}
+                aria-expanded={expanded}
+                aria-label="show more"
+              >
+                <ExpandMoreIcon />
+              </ExpandMore>
+            </CardActions>
+            <Collapse in={expanded} timeout="auto" unmountOnExit>
+              <CardContent>
+                <Typography paragraph>Method:</Typography>
+                <Typography paragraph>
+                  Heat 1/2 cup of the broth in a pot until simmering, add
+                  saffron and set aside for 10 minutes.
+                </Typography>
+                <Typography paragraph>
+                  Heat oil in a (14- to 16-inch) paella pan or a large, deep
+                  skillet over medium-high heat. Add chicken, shrimp and
+                  chorizo, and cook, stirring occasionally until lightly
+                  browned, 6 to 8 minutes. Transfer shrimp to a large plate and
+                  set aside, leaving chicken and chorizo in the pan. Add
+                  pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                  pepper, and cook, stirring often until thickened and fragrant,
+                  about 10 minutes. Add saffron broth and remaining 4 1/2 cups
+                  chicken broth; bring to a boil.
+                </Typography>
+                <Typography paragraph>
+                  Add rice and stir very gently to distribute. Top with
+                  artichokes and peppers, and cook without stirring, until most
+                  of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                  medium-low, add reserved shrimp and mussels, tucking them down
+                  into the rice, and cook again without stirring, until mussels
+                  have opened and rice is just tender, 5 to 7 minutes more.
+                  (Discard any mussels that don&apos;t open.)
+                </Typography>
+                <Typography>
+                  Set aside off of the heat to let rest for 10 minutes, and then
+                  serve.
+                </Typography>
+              </CardContent>
+            </Collapse>
+          </Card>
+        </div>
+      ))}
+    </div>
+  );
+}
 export default Home
